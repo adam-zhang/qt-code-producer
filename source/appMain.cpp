ac2a86d4cebe84919ddb1c7c82aa6038c444e01c
AdamZhang <mingpingzhang@163.com>
2017-02-24 17:35:54 +0800

    gui would be added.

--- source/appMain.cpp
+++ source/appMain.cpp
@@ -12,8 +12,9 @@ bool printHelp()
 		<< "\t--help show this message.\n"
 		<< "\t--app generate files which can build a basic application.\n"
 		<< "\t--gui generate files which can build a basic GUI application.\n"
-		<< "\t--class generate two files for a basic structure of a class\n"
-		<< "\t-c a short name for --class." 
+		<< "\t--class generate two files for a class with base class.\n"
+		<< "\t--qclass generate two files for a class which base class must be a class in Qt.\n"
+		<< "\t--baseclass generate two files for a class without base class.\n"
 		<< endl;
 	return true;
 }
@@ -41,10 +42,71 @@ bool generateApp()
 
 bool analyse(int argc, char** argv)
 {
-	if (argc == 2 || argc == 3)
+	if (argc == 2 || argc == 3 || argc == 4)
 		return true;
 	else
 		return printHelp();
+}
+
+void writeContent(const string& fileName, const string& content)
+{
+	fstream file;
+	file.open(fileName.c_str(), ios_base::in);
+	if (file.good())
+	{
+		file << content;
+		file.close();
+		return;
+	}
+}
+
+string getGuiMainContent()
+{
+	stringstream ss;
+	ss << "#include <QApplication>\n"
+		"#include \"MainWindow.h\"\n\n"
+		"int main(int argc, char** argv)\n"
+		"{\n"
+		"\tQApplication a(argc, argv);\n"
+		"\tMainWindow w;"
+		"w.show();"
+		"return a.exec();"
+		"}" << endl;
+	return ss.str();
+}
+
+string getMainWindowHeaderContent()
+{
+	stringstream ss;
+	ss << "#ifndef __MAIN_WINDOW__H\n"
+	      "#define __MAIN_WINDOW__H\n\n"
+	      "#include <QMainWindow>\n\n"
+	      "class MainWindow : public QMainWindow\n"
+	      "{\n"
+	      "public:\n"
+	      "\tMainWindow();\n"
+	      "\t~MainWindow();\n"
+	      "}\n"
+	      "#endif//__MAIN_WINDOW__H" << endl;
+	return ss.str();
+}
+
+string getGuiMainWindowContent()
+{
+	return "#include \"MainWindow.h\"\n\n"
+		"MainWindow::MainWindow() {}\n\n"
+		"MainWindow::~MainWindow() {}";
+
+}
+
+int generateGuiApp()
+{
+	string mainContent = getGuiMainContent();
+	writeContent("appMain.cpp", mainContent);
+	string guiHeaderContent = getMainWindowHeaderContent();
+	writeContent("MainWindow.h", guiHeaderContent);
+	string guiWindowContent = getGuiMainWindowContent();
+	writeContent("MainWindow.cpp", guiWindowContent);
 }
 
 int main(int argc, char** argv)
@@ -55,8 +117,8 @@ int main(int argc, char** argv)
 		return printHelp();
 	if (argc == 2 && strcmp(argv[1],"--app") == 0)
 		return generateApp();
-	//if (argc == 2 && strcmp(argv[1],"--gui") == 0)
-	//	return generateGuiApp();
+	if (argc == 2 && strcmp(argv[1],"--gui") == 0)
+		return generateGuiApp();
 	//if (argc == 3 && (strcmp(argv[1],"--class") == 0 || strcmp(argv[1],"-c") == 0)
 	//	return generateClass(argv[2]);
 	return 0;
